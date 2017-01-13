931ec1db6bee0a96ff82aeed1c7319aeef5f8a7f
Adam D Thomas <adam.thomas@anu.edu.au>
2017-01-13 17:35:19 +1100

    Added nearly empty bigelm_mcmc.py

---
+++ src/bigelm_mcmc.py
@@ -0,0 +1,25 @@
+from __future__ import print_function, division
+import numpy as np  # Core numerical library
+import emcee
+
+
+
+"""
+Adam D. Thomas 2015 - 2016
+
+
+
+"""
+
+
+
+def posterior():
+    """
+    Evaluate the posterior at a particular point in the parameter space
+    """
+    pass
+
+   
+
+
+# Note: look at starmodel.py in isochrones by TDMorton for inspiration
