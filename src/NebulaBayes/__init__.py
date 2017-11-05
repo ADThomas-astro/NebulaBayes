968294c07a922625c5264bc87c01c81de97edc94
Adam D. Thomas <adam.thomas@anu.edu.au>
2017-11-05 19:44:45 +1100

    Major package reorganisation

--- src/NebulaBayes/__init__.py
+++ src/NebulaBayes/__init__.py
@@ -7,7 +7,7 @@ Australian National University
 
 This module performs Bayesian parameter estimation.  The data are a set
 of emission line flux measurements with associated errors.  The model
-is a photoionisation model, varied in a grid over n=2 or more parameters,
+is a photoionisation model, varied in a grid over n=1 or more parameters,
 input as n-dimensional grids of fluxes for each emission line.  The model is
 for an HII region or AGN Narrow Line Region, for example.  The measured
 and modelled emission line fluxes are compared to calculate a "likelihood"
@@ -20,11 +20,9 @@ NebulaBayes is heavily based on IZI (Blanc+2015).
 """
 
 
-from .src.NB0_Main import NB_Model
+from .NB0_Main import NB_Model
 from ._version import __version__
 
 # N.B. The docstring at the top may be accessed interactively in ipython3 with:
 # >>> import NebulaBayes
 # >>> NebulaBayes?
-
-
